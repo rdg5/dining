56e2c1cf91c7c931160073450038e7e7a587c73d
sanyi <sandor.vass@tuta.io>
2023-07-26 18:36:34 +0200

    feat(): add role model

---
+++ backend/services/dining-service/data-access/models/role-model.ts
@@ -0,0 +1,35 @@
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from 'sequelize';
+import getDbConnection from './db-connection';
+
+export interface RoleModelFields
+  extends Model<
+    InferAttributes<RoleModelFields>,
+    InferCreationAttributes<RoleModelFields>
+  > {
+  id: CreationOptional<number>;
+  role: string;
+}
+
+export function getOrderModel() {
+  const roleModel = getDbConnection().define<RoleModelFields>(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      role: {
+        type: DataTypes.STRING,
+      },
+    },
+    { freezeTableName: true }
+  );
+  return roleModel;
+}
