ca83889e2213e98083e91aac174954e9fb89ec63
sanyi <sandor.vass@tuta.io>
2023-07-26 18:38:19 +0200

    feat(): add permission model

---
+++ backend/services/dining-service/data-access/models/permission-model.ts
@@ -0,0 +1,35 @@
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from 'sequelize';
+import getDbConnection from './db-connection';
+
+export interface PermissionModelFields
+  extends Model<
+    InferAttributes<PermissionModelFields>,
+    InferCreationAttributes<PermissionModelFields>
+  > {
+  id: CreationOptional<number>;
+  ability: string;
+}
+
+export function getOrderModel() {
+  const permissionModel = getDbConnection().define<PermissionModelFields>(
+    'User',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      ability: {
+        type: DataTypes.STRING,
+      },
+    },
+    { freezeTableName: true }
+  );
+  return permissionModel;
+}
