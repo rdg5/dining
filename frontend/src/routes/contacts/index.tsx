98a49b12d95c2bc56cc6c167eebe6e385246a1c6
sanyi <sandor.vass@tuta.io>
2023-08-11 16:39:23 +0200

    chore(): init frontend

---
+++ frontend/src/routes/contacts/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+	return(
+		<div>
+			<h1>These would be the contacts</h1>
+			<p>these are the contacts</p>
+		</div>
+	);
+});
